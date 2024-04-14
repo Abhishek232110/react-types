41292421400d1fd53124628370afe0e699b2a4fc
Abhishek Kumar <abhi7376862392@gmail.com>
2024-04-14 15:54:30 +0530

    change

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,19 @@
-import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className=" bg-indigo-400 text-white flex justify-center p-20">
+      <header>
+        <h2>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Vero
+          asperiores nam reiciendis ab possimus, quod consectetur praesentium?
+          Exercitationem ipsum a quam debitis vero porro temporibus placeat
+          perspiciatis, dolorem rerum minima.
+        </h2>
+        <h1>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Est eaque
+          repellat tempore ipsam. Illo at nemo aut possimus sit fugiat delectus,
+          voluptates adipisci quaerat tempora quis tenetur dolores obcaecati
+          officia.
+        </h1>
       </header>
     </div>
   );
